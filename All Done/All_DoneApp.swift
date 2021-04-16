7771871739bd46d642d739e8f4702c9ed3a90e9e
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-04-16 12:01:11 -0600

    Initial Commit

---
+++ All Done/All_DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_DoneApp.swift
+//  All Done
+//
+//  Created by Adam Garrett-Harris on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct All_DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
