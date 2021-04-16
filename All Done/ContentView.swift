1f117bf9eada8a5e1f06776cc042ca538fb7238b
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-04-16 13:35:59 -0600

    Make a 10 minute timer

--- All Done/ContentView.swift
+++ All Done/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isRunning: Bool = false
+    @State var endTime: Date? = nil
+    @State var timeRemaining: Double? = 600.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            TimerRow(isRunning: $isRunning, endTime: $endTime, timeRemaining: $timeRemaining)
+        }
     }
+}
+
+struct TimerRow: View {
+    @State var timer: Timer? = nil
+    @State var minutes: Int = 0
+    @State var seconds: Int = 0
+    
+    @Binding var isRunning: Bool
+    @Binding var endTime: Date?
+    @Binding var timeRemaining: Double?
+    
+    var secondsRemaining: Double {
+        if let timeRemaining = timeRemaining {
+            return timeRemaining
+        }
+        let seconds = endTime!.timeIntervalSince(Date())
+        return seconds < 0 ? 0.0 : seconds
+    }
+    
+    var body: some View {
+        HStack {
+            Text("\(minutes):\(padNumber(n: seconds)) until 5 minute warning")
+                .foregroundColor(secondsRemaining == 0 ? Color.red : Color("textColor"))
+            Spacer()
+            Button(action: {
+                if isRunning {
+                    stopTimer()
+                } else {
+                    startTimer()
+                }
+            }) {
+                Image(systemName: isRunning ? "pause.fill" : "play.fill")
+            }
+        }
+        .onAppear {
+            if let timeRemaining = timeRemaining {
+                seconds = Int(timeRemaining) % 60
+                minutes = Int(timeRemaining) / 60
+            }
+        }
+    }
+    
+    func startTimer(){
+        isRunning = true
+        endTime = Date(timeInterval: timeRemaining!, since: Date())
+        timeRemaining = nil
+
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true){ tempTimer in
+            let totalSeconds = secondsRemaining
+            seconds = Int(totalSeconds) % 60
+            minutes = Int(totalSeconds) / 60
+        }
+    }
+    
+    func stopTimer(){
+        isRunning = false
+        timeRemaining = secondsRemaining
+        endTime = nil
+        timer?.invalidate()
+        timer = nil
+    }
+
+}
+
+func padNumber(n: Int) -> String {
+    n < 10 ? "0\(n)" : "\(n)"
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
